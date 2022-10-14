f24dad6ef8672ace29aac45fdc02c00c692a025c
Ana11071994 <anand11mech@gmail.com>
2022-10-14 14:51:47 +0530

    India

---
+++ src/org/sample/Student.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class Student {
+	private void studentId() {
+		System.out.println("8743");
+		
+
+	}
+	private void studentName() {
+		System.out.println("balu");
+
+	}
+
+}
